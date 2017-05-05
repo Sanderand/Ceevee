2b317c0fc7cac2844ca5a9b6e7e2bdedca55a8b4
Andre <andre--sander@web.de>
2017-05-06 10:42:41 +1200

    closing dropdown on scroll

--- src/app/shared/components/dropdown/dropdown.component.ts
+++ src/app/shared/components/dropdown/dropdown.component.ts
@@ -1,4 +1,14 @@
-import { Component, ElementRef, HostBinding, HostListener, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ViewEncapsulation
+} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'cv-dropdown',
@@ -6,14 +16,24 @@ import { Component, ElementRef, HostBinding, HostListener, Input, ViewEncapsulat
     styleUrls: ['./dropdown.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class DropdownComponent {
+export class DropdownComponent implements OnChanges {
     @HostBinding('class.open')
     @Input() public open: boolean = false;
     @Input() public remainOnInnerClick: boolean = false;
+    @Input() public closeOnScroll: boolean = false;
 
     constructor (
       private _elementRef: ElementRef
     ) {}
+
+
+    public ngOnChanges (changes: SimpleChanges): void {
+        let { closeOnScroll } = changes;
+
+        if (closeOnScroll && closeOnScroll.currentValue && closeOnScroll.isFirstChange()) {
+            this.addScrollListener();
+        }
+    }
 
     @HostListener('body:click', ['$event'])
     public onClick ($event): void {
@@ -21,6 +41,18 @@ export class DropdownComponent {
             return;
         }
 
+        this.close();
+    }
+
+    private addScrollListener (): void {
+        let main = document.getElementsByTagName('main');
+
+        Observable
+            .fromEvent(main, 'scroll')
+            .subscribe(e => this.close());
+    }
+
+    private close (): void {
         if (this.open) {
             this.open = false;
         }
